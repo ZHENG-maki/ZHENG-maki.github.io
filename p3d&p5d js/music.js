2d2fd69b841630e8e2f0623b5146b1cd25c5f8c3
ZHENG-maki <49464381+ZHENG-maki@users.noreply.github.com>
2019-05-01 21:51:14 +0800

    ..
    ..

--- p3d&p5d js/music.js
+++ p3d&p5d js/music.js
@@ -1,3 +1,4 @@
+//右侧歌曲信息滑动栏
 $(function(){
 	var flag=0;
 	var p=document.getElementById("wz");
@@ -6,6 +7,7 @@ $(function(){
 		{
 			$("#music_xinxi").animate({width:"230px"},"slow");
 			$("#music_stay").animate({height:"220px"},"slow")
+			$("#music_fm1,#music_fm,.music_mz").css("display","block");
 			p.innerHTML="关闭歌曲信息";
 			flag=1;
 		}
@@ -13,8 +15,39 @@ $(function(){
 		{
 			$("#music_xinxi").animate({width:"0"},"slow");
 			$("#music_stay").animate({height:"275px"},"slow")
+			$("#music_fm1,#music_fm,.music_mz").css("display","none");
 			p.innerHTML="管理当前播放歌曲";
 			flag=0;
 		}
 	});
 });
+
+//歌曲点击播放
+$(function(){
+	var audio=document.getElementById("Music_bofang");
+	$("dt").click(function(){
+		audio.src=this.className;
+		audio.title=this.title;
+		audio.play();
+	});
+});
+
+//歌曲封面
+$(function(){
+	$("dt").click(function(){
+		var img=document.getElementById("music_fm");
+		var img_src=document.getElementsByClassName("p5d图片");
+		img.src=img_src[0].src;
+		$("#music_fm1").css("background","url(img/碟片.png) no-repeat right");
+	});
+});
+
+//歌曲名字
+$(function(){
+	$("dt").click(function(){
+		var mz=document.getElementsByClassName("music_mz");
+		var audio=document.getElementById("Music_bofang");
+		mz[0].innerHTML=audio.title;
+	});
+});
+
